a3076dc778825bf85c0f65265e881ab201ebb63d
Wayne Hsiao <chronicqazxc@gmail.com>
2019-04-04 01:31:38 +0800

    init

---
+++ MyService/MyService.h
@@ -0,0 +1,19 @@
+//
+//  MyService.h
+//  MyService
+//
+//  Created by Wayne Hsiao on 2019/4/4.
+//  Copyright © 2019 Wayne Hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyService.
+FOUNDATION_EXPORT double MyServiceVersionNumber;
+
+//! Project version string for MyService.
+FOUNDATION_EXPORT const unsigned char MyServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyService/PublicHeader.h>
+
+
